32f5e9385e8dd266ecdd53e327b8f980de4d0646
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-03-23 17:31:37 +0100

    Create "postsStore" state file

---
+++ client/src/stores/postsStore.js
@@ -0,0 +1,113 @@
+import { create } from "zustand";
+import axios from "axios";
+
+const postsStore = create((set) => ({
+  posts: null,
+
+  createForm: {
+    content: "",
+  },
+
+  updateForm: {
+    _id: null,
+    content: "",
+  },
+
+  fetchPosts: async () => {
+    // Fetch posts from the server
+    const resp = await axios.get("http://localhost:3000/posts");
+
+    // Set the posts state
+    set({ posts: resp.data.posts });
+  },
+
+  updateCFormField: (e) => {
+    const { name, value } = e.target;
+
+    set((state) => {
+      return {
+        createForm: {
+          ...state.createForm,
+          [name]: value,
+        },
+      };
+    });
+  },
+
+  createPost: async (e) => {
+    e.preventDefault();
+
+    // Create a post
+    const { createForm, posts } = postsStore.getState();
+    const resp = await axios.post("http://localhost:3000/posts", createForm);
+
+    // Update and clear form state
+    set({
+      posts: [...posts, resp.data.post],
+      createForm: { content: "" },
+    });
+  },
+
+  deletePost: async (_id) => {
+    const { posts } = postsStore.getState();
+
+    // Delete the post
+    const resp = await axios.delete(`http://localhost:3000/posts/${_id}`);
+
+    // Update state
+    const newPosts = posts.filter((post) => {
+      return post._id !== _id;
+    });
+
+    set({ posts: newPosts });
+  },
+
+  updateFormChange: (e) => {
+    const { name, value } = e.target;
+
+    set((state) => {
+      return {
+        updateForm: {
+          ...state.updateForm,
+          [name]: value,
+        },
+      };
+    });
+  },
+
+  fillUpdateForm: ({ _id, content }) => {
+    // Set state on update form
+    set({
+      updateForm: {
+        _id,
+        content,
+      },
+    });
+  },
+
+  updatePost: async (e) => {
+    e.preventDefault();
+
+    const {
+      updateForm: { _id, content },
+      posts,
+    } = postsStore.getState();
+
+    // Send the update request
+    const resp = await axios.put(`http://localhost:3000/posts/${_id}`, {
+      content,
+    });
+
+    // Update state
+    const newPosts = [...posts];
+    const postIndex = posts.findIndex((post) => {
+      return post._id === _id;
+    });
+    newPosts[postIndex] = resp.data.post;
+
+    // Clear form state
+    set({ posts: newPosts, updateForm: { _id: null, content: "" } });
+  },
+}));
+
+export default postsStore;
