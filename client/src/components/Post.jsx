c1dbe9e8381cd5944e6b12d802a11356bf9bfafc
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-14 16:49:15 +0200

    Convert file from js to jsx & add styling

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,18 +1,30 @@
-import postsStore from "../stores/postsStore";
+import PostsStore from "../stores/postsStore";
 
 export default function Post({ post }) {
-  const store = postsStore((store) => {
-    return {
-      deletePost: store.deletePost,
-      fillUpdateForm: store.fillUpdateForm,
-    };
-  });
+  const store = PostsStore((store) => ({
+    deletePost: store.deletePost,
+    fillUpdateForm: store.fillUpdateForm,
+  }));
 
   return (
-    <div key={post._id}>
-      <h3>{post.content}</h3>
-      <button onClick={() => store.deletePost(post._id)}>Delete post</button>
-      <button onClick={() => store.fillUpdateForm(post)}>Update post</button>
+    <div className="my-6 mx-2" key={post._id}>
+      <h3 className="p-4 rounded-md bg-gray-200 break-words overflow-scroll">
+        {post.content}
+      </h3>
+      <div>
+        <button
+          className="m-2 py-1 px-3 text-white text-sm font-bold bg-red-500 hover:bg-red-700 rounded-xl"
+          onClick={() => store.deletePost(post._id)}
+        >
+          Delete post
+        </button>
+        <button
+          className="m-2 py-1 px-3 text-white text-sm font-bold bg-blue-500 hover:bg-blue-700 rounded-xl"
+          onClick={() => store.fillUpdateForm(post)}
+        >
+          Update post
+        </button>
+      </div>
     </div>
   );
 }
