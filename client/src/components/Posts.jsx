f8bdc11db36ae631674a1ad0cb269b229cc75fe4
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-14 16:49:21 +0200

    Convert file from js to jsx & add styling

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -5,12 +5,14 @@ export default function Posts() {
   const store = postsStore((store) => {
     return {
       posts: store.posts,
-    }; 
+    };
   });
 
   return (
     <div>
-      <h2>Posts: </h2>
+      <hr className="border-solid border-1 border-slate-500"></hr>
+      <h2 className="m-2 text-center font-bold">Posts</h2>
+      <hr className="border-solid border-2 border-slate-500"></hr>
       {store.posts &&
         store.posts.map((post) => {
           return <Post post={post} key={post._id} />;
