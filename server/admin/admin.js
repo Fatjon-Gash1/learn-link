60de4642e1b94eaccffab033f188269a1dfce805
Fatjon Gashi <133712577+Fatjon-Gash1@users.noreply.github.com>
2024-04-15 00:54:48 +0200

    Update admin.js

--- server/admin/admin.js
+++ server/admin/admin.js
@@ -2,23 +2,20 @@ const configureAdminJS = async (app) => {
     // Dynamically import AdminJS
     const { default: AdminJS } = await import('adminjs').catch((err) => {
       console.error('Failed to import AdminJS:', err);
-      process.exit(1); // Exit the process if AdminJS import fails
+      process.exit(1);
     });
   
-    // Import AdminJSExpress
+    // Dynamically Import AdminJSExpress
     const { default: AdminJSExpress } = await import('@adminjs/express').catch((err) => {
       console.error('Failed to import AdminJSExpress:', err);
-      process.exit(1); // Exit the process if AdminJSExpress import fails
+      process.exit(1);
     });
   
-    // Create an instance of AdminJS with your desired options
     const admin = new AdminJS({
     });
   
-    // Build the AdminJS router
     const adminRouter = AdminJSExpress.buildRouter(admin);
   
-    // Mount the AdminJS router on your Express application
     app.use(admin.options.rootPath, adminRouter);
   };
   
