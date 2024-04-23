e17a73cfe40de1c10aa92a5effc857b6af459ce1
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-23 14:57:07 +0200

    Edit secondary database config

--- server/config/secDBconfig.js
+++ server/config/secDBconfig.js
@@ -3,28 +3,16 @@ if (process.nextTick.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
-// MySQL connection
-const mysql = require('mysql');
-
-function createMySQLPool() {
-    return mysql.createPool({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-    });
-}
-
 // MongoDB connection
 const mongoose = require('mongoose');
 
-async function connectToMongoDb() {
+async function connectToSecDB() {
     try {
         await mongoose.connect(process.env.MDB_URL);
-        console.log("Connected to MongoDB");
+        console.log("Connected to secondary database (MongoDB)");
     } catch (error) {
-        console.log("MongoDB connection failed", error);
+        console.log("Unable to connect to secondary database:", error);
     }
 }
 
-module.exports = { createMySQLPool, connectToMongoDb };
+module.exports = { connectToSecDB };
