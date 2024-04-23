6267cf742fb46ae0319ce69b0cc4cc3e1c3f2bbd
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-23 14:52:11 +0200

    Create route for dashboard

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import postsStore from "../stores/postsStore";
+import useAuthStore from "../stores/admin/authStore";
 import LoginForm from "./admin/LoginForm";
+import Dashboard from "./admin/Dashboard";
 import UserFeed from "./UserFeed";
 
 function App() {
   const store = postsStore();
+  const loggedIn = useAuthStore((state) => state.LoggedIn);
 
   // Hook to fetch posts
   useEffect(() => {
@@ -16,7 +19,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/home" element={<UserFeed />} />
-        <Route path="/adminlogin" Component={LoginForm} />
+        <Route path="/adminlogin" element={<LoginForm />} />
+        <Route path="/dashboard" element={loggedIn ? <Dashboard /> : <Navigate to="/adminlogin" />} />
       </Routes>
     </Router>
   );
