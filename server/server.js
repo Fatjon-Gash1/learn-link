36949edc535015980d99db0e45b285e6cf83019f
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-23 14:56:36 +0200

    Edit server routes

--- server/server.js
+++ server/server.js
@@ -1,41 +1,45 @@
 // Load env vars
 if (process.nextTick.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 
 // Import dependencies
-const express = require('express');
-const cors = require('cors');
-const { createMySQLPool, connectToMongoDb } = require("./config/databaseConfig"); 
+const express = require("express");
+const cors = require("cors");
+const primaryDB = require("./models/primary_db");
+const { checkPrimaryDBConn } = require("./models/primary_db/index");
+const { connectToSecDB } = require("./config/secDBconfig");
+const postsRoute = require("./routes/posts.route");
+const adminAuthRoute = require("./routes/adminAuth.route");
 const configureAdminJS = require("./admin/admin");
-const postController = require("./controllers/postsController");
 
-// Create express app 
-const app  = express();
+// Create express app
+const app = express();
 
 // Configure express app
 app.use(express.json());
 app.use(cors());
 
-// Create MySQL connection pool
-const mysqlPool = createMySQLPool();
+// Check Connection to primary database (MySQL)
+checkPrimaryDBConn();
 
-// Connect to mongoDB
-connectToMongoDb();
+// Connect to secondary database (MongoDB)
+connectToSecDB();
 
-// Routing
+// Routes
 app.get('/', (req, res) => {
-    res.json({message: "Server Active!"});
+  res.json({ message: 'Server Active' });
 });
 
-app.get('/posts', postController.fetchPosts);
-app.get('/posts/:id', postController.fetchPost);
-app.post('/posts', postController.createPost);
-app.put('/posts/:id', postController.updatePost);
-app.delete('/posts/:id', postController.deletePost);
+app.use("/posts", postsRoute);
+app.use("/adminlogin", adminAuthRoute); 
 
 // Configure AdminJS
 configureAdminJS(app);
 
 // Start server
-app.listen(process.env.PORT);
+primaryDB.sequelize.sync().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+  });
+});
