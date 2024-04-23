581f7150c62b55542f3d28f7cc193c1ef0c67325
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-04-23 14:55:31 +0200

    Create primary db files

---
+++ server/models/primary_db/User.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false 
+    }
+  });
+
+  return User;
+};
