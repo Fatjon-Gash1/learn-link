789478e0e182a206180cc7716610a03aad95a5dd
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-05-09 16:35:56 +0200

    Add token functionality

--- client/src/stores/admin/authStore.js
+++ client/src/stores/admin/authStore.js
@@ -1,40 +1,51 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const useAuthStore = create((set) => ({
-  username: "",
-  password: "",
-  LoggedIn: false,
-  setUsername: (username) => set((state) => ({ username })),
-  setPassword: (password) => set((state) => ({ password })),
 
-  sendData: async (e) => {
-    e.preventDefault();
-    const { username, password } = useAuthStore.getState();
+const useAuthStore = create((set) => {
+  const storedToken = localStorage.getItem("token");
+  return {
+    username: "",
+    password: "",
+    token: storedToken || null,
 
-    if (!username || !password) {
-      console.error("Username and password are required.");
-      return;
-    }
+    setUsername: (username) => set({ username }),
+    setPassword: (password) => set({ password }),
 
-    try {
-      const resp = await axios.post("http://localhost:3000/adminlogin/auth", {
-        username,
-        password,
-      });
-      console.log("Login successful:", resp.data);
-      set((state) => ({ LoggedIn: true }));
+    sendData: async (e) => {
+      e.preventDefault();
+      const { username, password } = useAuthStore.getState();
 
-    } catch (error) {
-      if (error.response) {
-        console.error("Server responded with error:", error.response.data);
-      } else if (error.request) {
-        console.error("No response received from server.");
-      } else {
-        console.error("Error while sending request:", error.message);
+      if (!username || !password) {
+        console.error("Username and password are required.");
+        return;
       }
-    }
-  },
-}));
+
+      try {
+        const resp = await axios.post("http://localhost:3000/adminlogin/auth", {
+          username,
+          password,
+        });
+        const { token } = resp.data;
+        localStorage.setItem("token", token);
+        set({ token });
+
+      } catch (error) {
+        if (error.response) {
+          console.error("Server responded with error:", error.response.data);
+        } else if (error.request) {
+          console.error("No response received from server.");
+        } else {
+          console.error("Error while sending request:", error.message);
+        }
+      }
+    },
+
+    logout: () => {
+      localStorage.removeItem("token");
+      set({ token: null });
+    },
+  };
+});
 
 export default useAuthStore;
