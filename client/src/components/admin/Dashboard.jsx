812b86320b8bec55e64e15ad8ea9e4b755674be3
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-05-09 16:34:45 +0200

    Refactor component

--- client/src/components/admin/Dashboard.jsx
+++ client/src/components/admin/Dashboard.jsx
@@ -1,7 +1,11 @@
+import useAuthStore from "../../stores/admin/authStore";
+
 const Dashboard = () => {
+  const { logout } = useAuthStore();
   return (
     <div>
       <h1>Welcome to the Dashboard</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 };
