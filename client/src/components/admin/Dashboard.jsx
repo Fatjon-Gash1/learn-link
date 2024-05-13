01b115a7cf06f2d3c975efe20b3dddfb28a09053
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-05-13 14:25:31 +0200

    Display user data & set interval for token expiration check

--- client/src/components/admin/Dashboard.jsx
+++ client/src/components/admin/Dashboard.jsx
@@ -1,10 +1,26 @@
+import React, { useEffect } from "react";
 import useAuthStore from "../../stores/admin/authStore";
 
 const Dashboard = () => {
-  const { logout } = useAuthStore();
+  const { name, surname, getData, logout } = useAuthStore();
+  const { detectTokenExpiry } = useAuthStore();
+
+  useEffect(() => {
+    detectTokenExpiry();
+    getData();
+
+    const interval = setInterval(detectTokenExpiry, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to the Dashboard</h1>
+      <h1>
+        Welcome to the Dashboard{" "}
+        <span className="text-sky-500">
+          {name} {surname}
+        </span>
+      </h1>
       <button onClick={logout}>Logout</button>
     </div>
   );
