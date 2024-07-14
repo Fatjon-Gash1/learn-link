1a1cc49b34dc8343e66a0c0b81f13b99749a13c4
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-07-14 16:25:48 +0200

    Edit env var

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,53 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import postsStore from "../stores/postsStore";
-import useAuthStore from "../stores/admin/authStore";
-import LoginForm from "./admin/LoginForm";
-import Dashboard from "./admin/Dashboard";
-import UserFeed from "./UserFeed";
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from 'react-router-dom';
+import postsStore from '../stores/postsStore';
+import useAuthStore from '../stores/admin/authStore';
+import LoginForm from './admin/LoginForm';
+import Dashboard from './admin/Dashboard';
+import UserFeed from './UserFeed';
 
 function App() {
-  console.log("env var react1: ", process.env.REACT_APP_BLA); // For testing
+    console.log('env var react1: ', process.env.REACT_APP_TEST); // For testing
 
-  const store = postsStore();
-  const { accessToken } = useAuthStore();
+    const store = postsStore();
+    const { accessToken } = useAuthStore();
 
-  useEffect(() => {
-    console.log("accessToken call from app.jsx:", accessToken); // For debugging
+    useEffect(() => {
+        console.log('accessToken call from app.jsx:', accessToken); // For debugging
 
-    store.fetchPosts();
-  }, []);
+        store.fetchPosts();
+    }, []);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/feed" element={<UserFeed />} />
-        <Route
-          exact
-          path="/adminlogin"
-          element={
-            accessToken ? <Navigate replace to={"/dashboard"} /> : <LoginForm />
-          }
-        />
-        <Route
-          exact
-          path="/dashboard"
-          element={
-            accessToken ? (
-              <Dashboard />
-            ) : (
-              <Navigate replace to={"/adminlogin"} />
-            )
-          }
-        />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/feed" element={<UserFeed />} />
+                <Route
+                    exact
+                    path="/adminlogin"
+                    element={
+                        accessToken ? (
+                            <Navigate replace to={'/dashboard'} />
+                        ) : (
+                            <LoginForm />
+                        )
+                    }
+                />
+                <Route
+                    exact
+                    path="/dashboard"
+                    element={
+                        accessToken ? (
+                            <Dashboard />
+                        ) : (
+                            <Navigate replace to={'/adminlogin'} />
+                        )
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
